72d613c0eafa43042bd674e0083916d8ee20ad65
Michael Chen <mchen092@gmail.com>
2018-03-19 14:56:32 -0400

    idk

--- spring-boot-server/src/main/java/com/revature/controller/SpaController.java
+++ spring-boot-server/src/main/java/com/revature/controller/SpaController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 // idk what this does
+//moar comment
 @Controller
 @RequestMapping("/")
 public class SpaController {
